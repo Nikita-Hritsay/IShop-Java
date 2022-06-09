6af62c5bfeb65a3a40c06dc306dc2d86ba735a1b
Nikita-Hritsay <hritsaynikita@gmail.com>
2022-06-09 12:02:25 +0300

    IS-3 test commit from another user to branch IS-3

--- src/main/java/com/courseproj/CourseProject/config/MailSend.java
+++ src/main/java/com/courseproj/CourseProject/config/MailSend.java
@@ -20,7 +20,7 @@ public class MailSend {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("hritsaynikita@gmail.com");
-        mailSender.setPassword("yourpassword");
+        mailSender.setPassword("yourpassword1");
 
         Properties properties = new Properties();
         properties.put("mail.transport.protocol", "smtp");
