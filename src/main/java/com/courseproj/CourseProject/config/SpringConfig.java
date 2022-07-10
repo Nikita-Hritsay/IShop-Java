3ccf5eb3a633380eed107f4eb0d5ce702b225ff6
MykytaHrytsai <hritsaynikita@gmail.com>
2022-07-10 18:28:07 +0300

    IS-24 test commit

--- src/main/java/com/courseproj/CourseProject/config/SpringConfig.java
+++ src/main/java/com/courseproj/CourseProject/config/SpringConfig.java
@@ -17,9 +17,9 @@ public class SpringConfig {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://192.168.31.52:3306/courseproject?useUnicode=true&serverTimezone=UTC");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/courseproject?useUnicode=true&serverTimezone=UTC");
         dataSource.setUsername("root");
-        dataSource.setPassword("****");
+        dataSource.setPassword("nikitahr");
 
         return dataSource;
     }
